a73822742ea2b4690098138fe53254d51c1b4a55
Joshua Empson <j.empson@livelinktechnology.net>
2019-12-12 15:25:54 +0000

    WORKINGgit statusgit statusgit statusgit statusgit statusgit status!

--- lib/image_album.rb
+++ lib/image_album.rb
@@ -16,17 +16,18 @@ class WMAlbum
     @options = options
     @options.each { |k,v| FileUtils.mkdir_p(v) if k.to_s.include?('dir') }
     @zipfile = File.join(@options[:output_dir], @options[:output_file])
-    abort('[WMTOOLS][IMAGEALBUM][ERR] Specify a filepath to JSON array of URLS with -f') unless @options[:urls]
+    abort('[WMTOOLS][IMGALBUM][ERR] Specify a filepath to JSON array of URLS with -f') unless @options[:urls]
   end
 
   def check_img(url)
-    puts("[WMTOOLS][IMAGEALBUM][LOG] Checking #{url}")
+    puts("[WMTOOLS][IMGALBUM][LOG] Checking #{url}")
     `curl -I "#{url}"` =~ /404 [Nn]ot [Ff]ound/ ? false : true
   end
 
   def download_img(url)
     puts("[WMTOOLS][IMGALBUM][LOG] Downloading image @ #{url}")
-    `wget -P #{@options[:temp_dir]} '#{url}'`
+    img_fn = [url.split('?').first.split('/').last.split('-').last.gsub(/\.[jJ][pP]([eE]?)[gG]/, ''), 'jpg'].join('.')
+    `wget -O "#{File.join(@options[:temp_dir], img_fn)}" "#{url}"`
   end
 
   def download_img_urls(check = false)
@@ -41,16 +42,16 @@ class WMAlbum
       end
       download_img(url)
     end
-    File.rm(@zipfile) if File.exist?(@zipfile)
-    `zip -r #{@zipfile} #{File.join(@options[:temp_dir], '')}`
-    puts("[WMTOOLS][IMAGEALBUM][LOG] Album downloaded to: #{@zipfile} \n")
+    File.delete(@zipfile) if File.exist?(@zipfile)
+    Dir.chdir(@options[:temp_dir]) { `zip -r #{@zipfile} *` }
+    puts("[WMTOOLS][IMGALBUM][LOG] Album downloaded to: #{@zipfile} \n")
     if failed_count != 0
-      puts("[WMTOOLS][IMAGEALBUM][LOG] Total missing images: #{failed_count.to_s} \n #{failed_images.to_s}")
-      puts('[WMTOOLS][IMAGEALBUM][INPUT] Would you like to retry downloading the missing images? [y/n]')
+      puts("[WMTOOLS][IMGALBUM][LOG] Total missing images: #{failed_count.to_s} \n #{failed_images.to_s}")
+      puts('[WMTOOLS][IMGALBUM][INPUT] Would you like to retry downloading the missing images? [y/n]')
       retry_ans = STDIN.gets.strip
       download_img_urls(missing_images) if retry_ans == 'y'
     end
-    Dir.rmdir(@options[:temp_dir])
+    FileUtils.rm_rf(@options[:temp_dir])
   end
 
   def check_img_urls
@@ -60,14 +61,14 @@ class WMAlbum
         missing_count += 1
         missing_images.push(url)
       end
-      puts("[WMTOOLS][IMAGEALBUM][LOG] Missing #{missing_count.to_s} images so far")
+      puts("[WMTOOLS][IMGALBUM][LOG] Missing #{missing_count.to_s} images so far")
     end
     puts('-------------------------------------------------')
-    puts("[WMTOOLS][IMAGEALBUM][LOG] Total missing images: #{missing_count.to_s}")
+    puts("[WMTOOLS][IMGALBUM][LOG] Total missing images: #{missing_count.to_s}")
     puts missing_images.to_s
     if missing_count != 0
       puts('')
-      puts('[WMTOOLS][IMAGEALBUM][INPUT] Would you like to retry the missing images? [y/n]')
+      puts('[WMTOOLS][IMGALBUM][INPUT] Would you like to retry the missing images? [y/n]')
       retry_ans = STDIN.gets.strip
       check_img_urls(missing_images) if retry_ans == 'y'
     end
